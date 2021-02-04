c4858d41d2111c708e7bfd4a1daf4766825583af
Shauna Dunn <shaunadunn1@gmail.com>
2021-02-04 14:38:25 -0500

    tests written, need to fill in code to test

---
+++ _tests_/Intern.test.js
@@ -0,0 +1,28 @@
+const Intern = require('./Intern')
+
+describe("Intern", () => {
+    describe("Initialization via constructor", () => {
+        it("should set school as its fourth parameter", () => {
+            const testSchool = "Yale";
+            const intern = new Intern("Harry", "okay@gmail.com", 5, testSchool);
+
+            expect(intern.school).toEqual(testSchool)
+        })
+    })
+
+    describe("Intern methods", () => {
+        it("getSchool() should return this.school", () => {
+            const testSchool = "Yale";
+            const intern = new Intern("Harry", "okay@gmail.com", 5, testSchool);
+
+            expect(intern.getSchool()).toEqual(testSchool);
+        })
+
+        it("getRole() should return 'Intern'", () => {
+            const testRole = "Intern";
+            const newIntern = new Intern("Emily", "okay@gmail.com", 8, "Harvard");
+
+            expect(newIntern.getRole()).toEqual(testRole);
+        })
+    })
+})
---
+++ _tests_/Manager.test.js
@@ -0,0 +1,28 @@
+const Manager = require('./Manager')
+
+describe("Manager", () => {
+    describe("Initialization via constructor", () => {
+        it("should set officeNumber as this.officeNumber", () => {
+            const testOfficeNumber = 14;
+            const manager = new Manager("Victoria", "okay@gmail.com", 4, testOfficeNumber);
+            
+            expect(manager.officeNumber).toEqual(testOfficeNumber)
+        })
+        })
+
+    describe("Manager methods", () => {
+        it("getRole() should return 'Manager", () => {
+            const testRole = "Manager";
+            const manager = new Manager("Francis", "okay@gmail.com", 2, 12);
+
+            expect(manager.getRole()).toEqual(testRole);
+        })
+
+        it("getofficeNumber() should return this.officeNumber", () => {
+            const testOfficeNumber = 32;
+            const manager = new Manager("Mark", "okay@gmail.com", 9, testOfficeNumber);
+
+            expect(manager.getofficeNumber()).toEqual(testOfficeNumber)
+        })
+    })
+    })
